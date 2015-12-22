fc0da0352180e476a4debaa632cc28f46e2dbe07
Andreas Bade <andreas.bade@identpro.de>
2015-12-22 13:59:48 +0100

    added new task for downloading rollbar snippets

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,9 @@ var src = ['./ng-rollbar.js'];
 var dst = './ng-rollbar.js';
 
 var lodash = require('lodash');
+var https = require('https');
+var fs = require('fs'),
+byline = require('byline');
 
 module.exports = function(grunt) {
 
@@ -12,10 +15,11 @@ module.exports = function(grunt) {
     },
     release: {
       options: {
-        npm: false,
+        npm: true,
         npmtag: false,
         commit: false,
-        file: 'bower.json',
+        file: 'package.json',
+        additionalFiles: ['bower.json'],
         beforeBump: ['fail', 'jshint'],
         afterBump: [
           'ngAnnotate',
@@ -69,4 +73,48 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-git');
 
   grunt.registerTask('default', ['jshint']);
+
+  grunt.registerTask('update-lib', 'integrate a new rollbar client lib version', function(version) {
+    // Tell grunt this task is asynchronous.
+    var done = this.async();
+
+    grunt.log.writeln("starting update-lib with version: " + version);
+    var url = "https://raw.githubusercontent.com/rollbar/rollbar.js/v" + version + "/dist/rollbar.snippet.js";
+    grunt.log.writeln("requesting '" + url + "' ...");
+    var lib = "";
+    https.get(url, function(res) {
+      grunt.log.writeln("Got response: " + res.statusCode);
+      res.on('data', function(snippet) {
+
+        var stream = byline(fs.createReadStream('./ng-rollbar.js', { encoding: 'utf8' }), { keepEmptyLines: true });
+        var newFileContent = "";
+
+        var waiting = false;
+        stream.on('data', function(line) {
+          if(line.indexOf("/* rollbar client lib end */") > -1) {
+            grunt.log.writeln("found /* rollbar client lib end */")
+            waiting = false;
+          }
+
+          if(!waiting) { newFileContent += line + "\n" }
+
+          if(line.indexOf("/* rollbar client lib start */") > -1) {
+            grunt.log.writeln("found /* rollbar client lib start */")
+            grunt.log.writeln("inserting new lib snippet")
+            waiting = true;
+            newFileContent += '        // ' + url + "\n"
+            newFileContent += '        ' + snippet + "\n"
+          }
+        });
+
+        stream.on('end', function() {
+          grunt.log.writeln("overwriting ng-rollbar.js with new lib");
+          fs.writeFile("./ng-rollbar.js", newFileContent.trim() + "\n");
+          done();
+        });
+      });
+    }).on('error', function(e) {
+      grunt.log.writeln("Got error: " + e.message);
+    });
+  });
 }
