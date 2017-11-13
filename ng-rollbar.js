be2080c684a5f903320f42d4aaee798d718deb2e
Andi Bade <andi.bade@gmail.com>
2017-11-13 21:12:29 +0100

    integrated rollbar snippet in version 2.3.1

---
+++ ng-rollbar.js
@@ -0,0 +1,140 @@
+(function(angular){
+  angular.module('tandibar/ng-rollbar', []);
+
+  angular.module('tandibar/ng-rollbar').config(['$provide', function($provide) {
+    $provide.decorator('$exceptionHandler', ['$delegate', '$injector', '$window', function($delegate, $injector, $window) {
+      return function (exception, cause) {
+        if($window.Rollbar) {
+          $window.Rollbar.error(exception, {cause: cause}, function(err, data) {
+            var $rootScope = $injector.get('$rootScope');
+            $rootScope.$emit('rollbar:exception', {
+              exception: exception,
+              err: err,
+              data: data.result
+            });
+          });
+        }
+        $delegate(exception, cause);
+      };
+    }]);
+  }]);
+
+  angular.module('tandibar/ng-rollbar').provider('Rollbar', function RollbarProvider() {
+    var rollbarProvider = this;
+    var rollbarActivated = true;
+
+    this.init = function(config) {
+      var _rollbarConfig = config;
+      if (rollbarActivated) {
+        /* jshint ignore:start */
+        /* rollbar client lib start */
+        // https://raw.githubusercontent.com/rollbar/rollbar.js/v2.3.1/dist/rollbar.snippet.js
+        estEventTarget,XMLHttpRequestUpload".split(",");for(n=0;n<a.length;++n)t=a[n],r[t]&&r[t].prototype&&l(o,r[t].prototype,e)}}function l(r,o,e){if(o.hasOwnProperty&&o.hasOwnProperty("addEventListener")){for(var n=o.addEventListener;n._rollbarOldAdd&&n.belongsToShim;)n=n._rollbarOldAdd;var t=function(o,e,t){n.call(this,o,r.wrap(e),t)};t._rollbarOldAdd=n,t.belongsToShim=e,o.addEventListener=t;for(var a=o.removeEventListener;a._rollbarOldRemove&&a.belongsToShim;)a=a._rollbarOldRemove;var l=function(r,o,e){a.call(this,r,o&&o._rollbar_wrapped||o,e)};l._rollbarOldRemove=a,l.belongsToShim=e,o.removeEventListener=l}}r.exports={captureUncaughtExceptions:e,captureUnhandledRejections:t,wrapGlobals:a}},function(r,o){"use strict";function e(r,o){this.impl=r(o,this),this.options=o,n(e.prototype)}function n(r){for(var o=function(r){return function(){var o=Array.prototype.slice.call(arguments,0);if(this.impl[r])return this.impl[r].apply(this.impl,o)}},e="log,debug,info,warn,warning,error,critical,global,configure,handleUncaughtException,handleUnhandledRejection,_createItem,wrap,loadFull,shimId,captureEvent,captureDomContentLoaded,captureLoad".split(","),n=0;n<e.length;n++)r[e[n]]=o(e[n])}e.prototype._swapAndProcessMessages=function(r,o){this.impl=r(this.options);for(var e,n,t;e=o.shift();)n=e.method,t=e.args,this[n]&&"function"==typeof this[n]&&("captureDomContentLoaded"===n||"captureLoad"===n?this[n].apply(this,[t[0],e.ts]):this[n].apply(this,t));return this},r.exports=e},function(r,o){"use strict";r.exports=function(r){return function(o){if(!o&&!window._rollbarInitialized){r=r||{};for(var e,n,t=r.globalAlias||"Rollbar",a=window.rollbar,l=function(r){return new a(r)},i=0;e=window._rollbarShims[i++];)n||(n=e.handler),e.handler._swapAndProcessMessages(l,e.messages);window[t]=n,window._rollbarInitialized=!0}}}}]);
+        /* rollbar client lib end */
+        /* jshint ignore:end */
+      }
+    };
+
+    this.deinit = function () {
+      rollbarActivated = false;
+    };
+
+    getter.$inject = ['$log', '$window'];
+    function getter($log, $window) {
+
+      function _bindRollbarMethod(methodName) {
+        return function() {
+          $window.Rollbar[methodName].apply($window.Rollbar, arguments);
+        };
+      }
+
+      var service = {
+        Rollbar: logInactiveMessage,
+
+        configure: logInactiveMessage,
+
+        critical: logInactiveMessage,
+        error: logInactiveMessage,
+        warning: logInactiveMessage,
+        info: logInactiveMessage,
+        debug: logInactiveMessage,
+
+        scope: logInactiveMessage,
+
+        verbose: logInactiveMessage,
+        enable: logInactiveMessage,
+        disable: logInactiveMessage
+      };
+
+      if (rollbarActivated) {
+        service.Rollbar = $window.Rollbar;
+
+        // bind the native Rollbar methods
+        service.configure = _bindRollbarMethod('configure');
+        service.critical = _bindRollbarMethod('critical');
+        service.error = _bindRollbarMethod('error');
+        service.warning = _bindRollbarMethod('warning');
+        service.info = _bindRollbarMethod('info');
+        service.debug = _bindRollbarMethod('debug');
+        service.scope = _bindRollbarMethod('scope');
+
+        service.verbose = function (boolean) {
+          if (boolean === undefined) { boolean = true; }
+          $window.Rollbar.configure({ verbose: boolean });
+        };
+
+        service.enable = function () {
+          $window.Rollbar.configure({ enabled: true });
+        };
+
+        service.disable = function () {
+          $window.Rollbar.configure({ enabled: false });
+        };
+      }
+
+      function logInactiveMessage() {
+        $log.warn("Rollbar is deactivated");
+      }
+
+      return service;
+    }
+
+    this.$get = getter;
+  });
+
+})
+(angular);
+nfigure = _bindRollbarMethod('configure');
+        service.critical = _bindRollbarMethod('critical');
+        service.error = _bindRollbarMethod('error');
+        service.warning = _bindRollbarMethod('warning');
+        service.info = _bindRollbarMethod('info');
+        service.debug = _bindRollbarMethod('debug');
+        service.scope = _bindRollbarMethod('scope');
+
+        service.verbose = function (boolean) {
+          if (boolean === undefined) { boolean = true; }
+          $window.Rollbar.configure({ verbose: boolean });
+        };
+
+        service.enable = function () {
+          $window.Rollbar.configure({ enabled: true });
+        };
+
+        service.disable = function () {
+          $window.Rollbar.configure({ enabled: false });
+        };
+      }
+
+      function logInactiveMessage() {
+        $log.warn("Rollbar is deactivated");
+      }
+
+      return service;
+    }
+
+    this.$get = getter;
+  });
+
+})
+(angular);
